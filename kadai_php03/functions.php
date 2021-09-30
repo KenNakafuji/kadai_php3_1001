4a75806eb5bf97da4c44a1761a9fc7f7475faeb7
10_中藤健_M <87561641+KenNakafuji@users.noreply.github.com>
2021-10-01 07:59:34 +0900

    Add files via upload

---
+++ kadai_php03/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+// functions.php
+
+function connect_to_db()
+{
+  $dbn='mysql:dbname=gsacs_d03_10;charset=utf8;port=3306;host=localhost';
+  $user = 'root';
+  $pwd = '';
+  try {
+    return new PDO($dbn, $user, $pwd);
+  } catch (PDOException $e) {
+    exit('dbError:'.$e->getMessage());
+  }
+}
+
